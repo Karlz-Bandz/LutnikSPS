52c95d723a478d13c00c3490a2f695cd9386f95f
Karlz-Bandz <macbook@MelYoung.lan>
2025-08-02 12:06:44 +0200

    Social links

--- services.js
+++ services.js
@@ -7,14 +7,14 @@ class Service {
 }
 
 const services = [
-  new Service("Pełna regulacja (setup)", "Korekta siodełka, ustawienie akcji strun, krzywizny gryfu, menzury, konserwacja podstrunnicy, wymiana strun, dokręcenie luźnych elementów, czyszczenie, diagnoza problemów, korekta ustawień uwzględniająca indywidualne styl gry i potrzeby każdego klienta.", "200 – 300 PLN"),
-  new Service("Szlif progów + setup", "", "500-600 PLN"),
-  new Service("Wymiana progów + setup", "Dokładna wycena po oględzinach instrumentu oraz wyboru części.", "Od 900 PLN"),
-  new Service("Wymiana przetworników", "", "od 50 PLN/sztuka"),
-  new Service("Wymiana przełącznika", "+ cena przełącznika", "50-80 PLN"),
-  new Service("Ekranowanie + wymiana przewodów", "", "150-250 PLN"),
+  new Service("Pelna regulacja (setup)", "Korekta siodełka, ustawienie akcji strun, krzywizny gryfu, menzury, konserwacja podstrunnicy, wymiana strun, dokręcenie luźnych elementów, czyszczenie, diagnoza problemów, korekta ustawień uwzględniająca indywidualne styl gry i potrzeby każdego klienta.", "200 – 300 PLN"),
+  new Service("Szlif progow + setup", "", "500-600 PLN"),
+  new Service("Wymiana progow + setup", "Dokładna wycena po oględzinach instrumentu oraz wyboru części.", "Od 900 PLN"),
+  new Service("Wymiana przetwornikow", "", "od 50 PLN/sztuka"),
+  new Service("Wymiana przelacznika", "+ cena przełącznika", "50-80 PLN"),
+  new Service("Ekranowanie + wymiana przewodow", "", "150-250 PLN"),
   new Service("Wymiana gniazda jack", "+ cena gniazda", "50 PLN"),
-  new Service("Wymiana siodełka", "Kość/Tusq/Mosiądz", "150-220 PLN")
+  new Service("Wymiana siodelka", "Kość/Tusq/Mosiądz", "150-220 PLN")
 ];
 
 function renderCards(services) {
